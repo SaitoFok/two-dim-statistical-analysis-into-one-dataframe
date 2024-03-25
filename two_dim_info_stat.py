30a42f6dd872cf1611145a8df36065e5eb843b8b
SaitoFok <95642834+SaitoFok@users.noreply.github.com>
2024-03-25 22:01:58 +0800

    Update two_dim_info_stat.py

--- two_dim_info_stat.py
+++ two_dim_info_stat.py
@@ -6,29 +6,27 @@ output_path = "" # 输出文件路径，需要修改
 
 import pandas as pd
 df = pd.read_excel(input_path)
-df_suxing_gouci = df.groupby([A, B])[B].count().unstack().fillna(0) # 
-df_suxing_gouci["总计"] = df_suxing_gouci.sum(axis=1)
-df_suxing_gouci["占比"] = df_suxing_gouci["总计"] / df_suxing_gouci["总计"].sum()
-df_suxing_gouci["占比"] = df_suxing_gouci["占比"].apply(lambda x: format(x, ".2%"))
+df_new = df.groupby([A, B])[B].count().unstack().fillna(0) 
+
+# 计算A维度的总计和占比
+df_new["总计"] = df_new.sum(axis=1)
+df_new["占比"] = df_new["总计"] / df_new["总计"].sum()
+df_new["占比"] = df_new["占比"].apply(lambda x: format(x, ".2%"))
 # 转置DataFrame
-df_suxing_gouci = df_suxing_gouci.T
+df_new = df_new.T
 
 # 获取除"总计"和"占比"行以外的所有行
-rows = [row for row in df_suxing_gouci.index if row not in ['总计', '占比']]
-df_temp = df_suxing_gouci.loc[rows]
+rows = [row for row in df_new.index if row not in ['总计', '占比']]
+df_temp = df_new.loc[rows]
 
-# 计算素性维度的总计
+# 计算B维度的总计和占比
 df_temp['总计'] = df_temp.sum(axis=1)
-
-# 计算素性维度的占比 
 total_sum = df_temp['总计'].sum()
 df_temp['占比'] = df_temp['总计'] / total_sum
 df_temp['占比'] = df_temp['占比'].apply(lambda x: format(x, '.2%'))
 
 # 将"总计"和"占比"两行合并回原DataFrame
-df_suxing_gouci = df_suxing_gouci.merge(df_temp[['总计', '占比']], how='left', left_index=True, right_index=True)
+df_new = df_new.merge(df_temp[['总计', '占比']], how='left', left_index=True, right_index=True)
 # 转置回来
-df_suxing_gouci = df_suxing_gouci.T
-df_suxing_gouci.to_excel(output_path, index=True)
-
-df_suxing_gouci
+df_new = df_new.T
+df_new.to_excel(output_path, index=True)
